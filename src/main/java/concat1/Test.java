833aa7df55dbc26819053887f3bb2602f9131a82
Yarragunta Thanmai <yarthanm@publicisgroupe.net>
2022-11-29 22:22:21 +0530

    added

---
+++ src/main/java/concat1/Test.java
@@ -0,0 +1,13 @@
+package concat1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstname = "Thanmai";
+		String lastname = "reddy";
+		System.out.println("Hello "+firstname+lastname);
+
+	}
+
+}
